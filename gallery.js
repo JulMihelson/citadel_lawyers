5195bbd2a9e33e23261a5cae855e1d4eba974745
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2024-01-09 13:56:03 +0200

    desktop version for check

--- gallery.js
+++ gallery.js
@@ -1,32 +1,65 @@
-var $cell = $(".card");
+// var $cell = $(".card");
 
-//open and close card when clicked on card
+// //open and close card when clicked on card
+// $cell.find(".js-expander").click(function () {
+//   var $thisCell = $(this).closest(".card");
+
+//   if ($thisCell.hasClass("is-collapsed")) {
+//     $cell.not($thisCell).removeClass("is-expanded").addClass("is-collapsed");
+//     // .addClass("is-inactive");
+//     $thisCell.removeClass("is-collapsed").addClass("is-expanded");
+
+//     // if ($cell.not($thisCell).hasClass("is-inactive")) {
+//     //   //do nothing
+//     // } else {
+//     //   $cell.not($thisCell).addClass("is-inactive");
+//     // }
+//   } else {
+//     $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+//     $cell.not($thisCell).removeClass("is-inactive");
+//   }
+// });
+
+// //close card when click on cross
+// $cell.find(".js-collapser").click(function () {
+//   var $thisCell = $(this).closest(".card");
+
+//   $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+//   // $cell.not($thisCell).removeClass("is-inactive");
+// });
+var $cell = $(".card");
+var $practiciesContainer = $(".practicies"); // Замініть це на правильний селектор вашого контейнера practicies
+
 $cell.find(".js-expander").click(function () {
   var $thisCell = $(this).closest(".card");
 
   if ($thisCell.hasClass("is-collapsed")) {
-    $cell
-      .not($thisCell)
-      .removeClass("is-expanded")
-      .addClass("is-collapsed")
-      .addClass("is-inactive");
+    $cell.not($thisCell).removeClass("is-expanded").addClass("is-collapsed");
     $thisCell.removeClass("is-collapsed").addClass("is-expanded");
 
-    if ($cell.not($thisCell).hasClass("is-inactive")) {
-      //do nothing
-    } else {
-      $cell.not($thisCell).addClass("is-inactive");
-    }
+    // Підняти відкриту карту на центр viewport
+    var offsetTop =
+      $thisCell.offset().top -
+      ($(window).height() - $thisCell.outerHeight()) / 3;
+    $("html, body").animate({ scrollTop: offsetTop }, 300);
   } else {
     $thisCell.removeClass("is-expanded").addClass("is-collapsed");
-    $cell.not($thisCell).removeClass("is-inactive");
+
+    // При закритті карти, центрувати viewport відносно контейнера practicies
+    var containerOffsetTop =
+      $practiciesContainer.offset().top -
+      ($(window).height() - $practiciesContainer.outerHeight()) / 3;
+    $("html, body").animate({ scrollTop: containerOffsetTop }, 300);
   }
 });
 
-//close card when click on cross
 $cell.find(".js-collapser").click(function () {
   var $thisCell = $(this).closest(".card");
-
   $thisCell.removeClass("is-expanded").addClass("is-collapsed");
-  $cell.not($thisCell).removeClass("is-inactive");
+
+  // При закритті карти, центрувати viewport відносно контейнера practicies
+  var containerOffsetTop =
+    $practiciesContainer.offset().top -
+    ($(window).height() - $practiciesContainer.outerHeight()) / 3;
+  $("html, body").animate({ scrollTop: containerOffsetTop }, 300);
 });
