c38d4008e714f7044348e4b9c7d3b44d248fe7a0
jul.mihelson@gmail.com <125132083+JulMihelson@users.noreply.github.com>
2024-01-12 12:08:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-const refs = {
-  contentCard: document.querySelector(".tab-content"),
-  tabLink: document.querySelectorAll(".tab-link"),
-  // cardDevelopment: document.querySelector("#development"),
-  //   cardCorporative: document.querySelector("#corporative"),
-  //   cardTax: document.querySelector("#tax"),
-  //   cardIt: document.querySelector("#it"),
-  // closeCardDevelopment: document.querySelector("#development"),
-  // closeCardCorporative: document.querySelector("#corporative"),
-  // closeCardTax: document.querySelector("#tax"),
-  // closeCardIt: document.querySelector("#it"),
-};
-// window.addEventListener("click", closeCard);
-refs.tabLink.forEach((element) => {
-  element.addEventListener("click", navClick);
-});
-// refs.closeCardDevelopment.addEventListener("click", closeCard);
-// refs.closeCardCorporative.addEventListener("click", closeCard);
-// refs.closeCardTax.addEventListener("click", closeCard);
-// refs.closeCardIt.addEventListener("click", closeCard);
-let activeTab = null;
-
-function navClick(event) {
-  event.preventDefault();
-  console.log(
-    "navLink",
-    event.target,
-    event.currentTarget,
-    event.currentTarget
-  );
-}
-window.document;
-function closeCard(event) {
-  const target = event.target;
-  if (target === refs.contentCard) {
-  }
-  console.log(target, "target");
-  console.log(event.currentTarget);
-  console.log(refs.contentCard, "contentCard");
-  console.log(window.document, "wind");
-}
-
-// () => {
-//   const refs = {
-//     card: document.querySelector(".tab-link"),
-//   };
-//   for (let i = 0; i < refs.card.length; i++) {
-//     card[i].addEventListener("click", () => {
-//       this.classList.toggle("active");
-//       let content = this.nextElementSibling;
-//       if (content.style.maxHeight) {
-//         content.style.maxHeight = null;
-//       } else {
-//         content.style.maxHeight = content.scrollHeight + "px";
-//       }
-//     });
-//   }
+// const refs = {
+//   contentCard: document.querySelector(".tab-content"),
+//   tabLink: document.querySelectorAll(".tab-link"),
 // };
+
+// refs.tabLink.forEach((element) => {
+//   element.addEventListener("click", navClick);
+// });
+
+// let activeTab = null;
+
+// function navClick(event) {
+//   event.preventDefault();
+//   console.log(
+//     "navLink",
+//     event.target,
+//     event.currentTarget,
+//     event.currentTarget
+//   );
+// }
+// window.document;
+// function closeCard(event) {
+//   const target = event.target;
+//   if (target === refs.contentCard) {
+//   }
+//   console.log(target, "target");
+//   console.log(event.currentTarget);
+//   console.log(refs.contentCard, "contentCard");
+//   console.log(window.document, "wind");
+// }
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById(".video-bg");
+
+  document.addEventListener("click", function playVideo() {
+    video.play();
+    document.removeEventListener("click", playVideo);
+  });
+});
